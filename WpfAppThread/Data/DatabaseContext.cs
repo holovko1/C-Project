de045e692da52d0e6d7917d9b0bea984c88aa000
Golov_ng03@student.itstep.org <Golov_ng03@student.itstep.org>
2024-04-06 18:21:57 +0300

    Cancel/Close + DB

---
+++ WpfAppThread/Data/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppThread.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=app.db;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
--- WpfAppThread/MainWindow.xaml.cs
+++ WpfAppThread/MainWindow.xaml.cs
@@ -11,9 +11,6 @@ using System.Windows.Shapes;
 
 namespace WpfAppThread
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -21,30 +18,62 @@ namespace WpfAppThread
             InitializeComponent();
         }
 
+        private Thread thread;
+        private bool isPaused = false;
+        private volatile bool isClosing = false;
+
         private void btnRun_Click(object sender, RoutedEventArgs e)
         {
-            //MessageBox.Show("Add items " + txtCount.Text);
+            isClosing = false;
             btnRun.IsEnabled = false;
             double count = double.Parse(txtCount.Text);
-            Thread thread = new Thread(() =>
+            thread = new Thread(() => 
                 InsertItems(count));
-
             thread.Start();
         }
 
         private void InsertItems(double count)
         {
             Dispatcher.Invoke(() => { pbStatus.Maximum = count; });
-            
             for (int i = 0; i < count; i++)
             {
                 Dispatcher.Invoke(() => { pbStatus.Value = i + 1; });
-                
+                while (isPaused)
+                {
+                    Thread.Sleep(100);
+                }
+                if (isClosing)
+                {
+                    Dispatcher.Invoke(() => { pbStatus.Value = 0; });
+                    MessageBox.Show("Скасовано");
+                    break;
+                }    
                 Thread.Sleep(100);
             }
-
             Dispatcher.Invoke(() => { btnRun.IsEnabled = true; });
-            
+            if (!isClosing)
+                    MessageBox.Show("Виконано");
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            isPaused = !isPaused;
+            btnPause.Content = isPaused ? "Продовжити" : "Пауза";
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            isClosing = true;
+            if (thread.IsAlive)
+            {
+                thread.Join();
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            isClosing = true;
         }
     }
 }
